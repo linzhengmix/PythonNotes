730ba96698fb255ca880532fd23445edafff3a01
Linzheng <759967211@qq.com>
2019-04-08 11:26:28 +0800

    add comments

--- xkcd/downloader.py
+++ xkcd/downloader.py
@@ -1,21 +1,32 @@
 # encoding: utf-8	
 
- """	
-date: 2019/04/01/15/08	
- """	
+
+"""
+date: 2019/04/01/15/08
+"""
+
+
+"""
+加载所需要的包
+"""
 import os,sys	
 import requests	
 from bs4 import BeautifulSoup	
 
- sys.path.append(os.path.dirname(os.path.abspath("__file__")))	
+'''
+创建保存图片的文件夹
+'''
+sys.path.append(os.path.dirname(os.path.abspath("__file__")))
 if not os.path.exists('images'):	
-    os.mkdir('images')	
-else:	
-    pass	
+    os.mkdir('images')
+else:
+    pass
+url = "https://xkcd.com/"
 
- url = "https://xkcd.com/"	
-
- while not url.endswith("#"):	
+'''
+爬取数据
+'''
+while not url.endswith("#"):
     res = requests.get(url)	
     res.raise_for_status()	
     soup = BeautifulSoup(res.text,'lxml')	
@@ -26,15 +37,15 @@ else:
         comicUrl = 'https:'+ comicItem[0].get('srcset')	
         comicUrl = comicUrl.split(" ")[0]	
 
-         res_d = requests.get(comicUrl)	
+        res_d = requests.get(comicUrl)
         res_d.raise_for_status()	
         imageFile = open(os.path.join('images',os.path.basename(comicUrl)),'wb')	
         for item in res_d.iter_content(10000):	
             imageFile.write(item)	
         imageFile.close()	
 
-     preLink = soup.select('a[rel="prev"]')[0]	
-    url = 'https://xkcd.com/' + preLink.get('href')	
-    print(url)	
+    preLink = soup.select('a[rel="prev"]')[0]
+    url = 'https://xkcd.com/' + preLink.get('href')
+    print(url)
 
- print("done")
+print("done")
